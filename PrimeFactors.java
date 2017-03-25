5bffd5434a1f4c40a97a94545ad64ae96ee5948f
lexiht <hazukitran@gmail.com>
2017-03-25 18:24:58 +0700

    Completed Prime factors, WIP draw diamond and diamond with name

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class PrimeFactors {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { generatePrimeFactors(30);}
 
+    private static void generatePrimeFactors(int number) {
+        for(int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                System.out.print(i + ",");
+                number = number / i;
+            }
+        }
     }
 }
--- TriangleDiamond.java
+++ TriangleDiamond.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.awt.*;
+
 public class TriangleDiamond {
 
     public static void main(String[] args) {
-	    printOneAsterisk();
+        printOneAsterisk();
         printVerticalLine(3);
         printRightTriangle(3);
         printIsoscelesTriangle(3);
+        printDiamond(3);
+        printDiamondWithName(3);
         printHorizontalLine(8);
     }
 
@@ -36,12 +40,68 @@ public class TriangleDiamond {
     }
 
     private static void printIsoscelesTriangle(int number) {
-        String result = "*";
-        // Need to center this
-        for (int i = 0; i < number; i++) {
-            System.out.println(result);
-            result += "**";
+        int i, j, k;
 
+        for (i = 0; i < number; i++) {
+            for (j = 0; j < number - i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 0; k < i * 2 + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void printDiamond(int number) {
+        int i, j, k;
+
+        for (i = 0; i < number; i++) {
+            for (j = 0; j < number - i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 0; k < i * 2 + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (i = number - 1; i >= 0; i--) {
+            for (j = number - 1; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (k = number - 2; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void printDiamondWithName(int number) {
+        int i, j, k;
+
+        for (i = 0; i < number; i++) {
+            for (j = 0; j < number - i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 0; k < i * 2 + 1; k++) {
+                if (i == number - 1) {
+                    System.out.print("~Lexi~");
+                    break;
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+        for (i = number - 1; i >= 0; i--) {
+            for (j = number - 1; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (k = number - 2; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
         }
     }
 }
